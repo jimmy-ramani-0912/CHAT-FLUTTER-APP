3626a954c57166b143cb9e5e33812187a4109bc7
20IT120-JIMMY <20it120@charusat.edu.in>
2022-03-12 23:00:32 +0530

    flutter app

--- lib/group_chats/group_chat_screen.dart
+++ lib/group_chats/group_chat_screen.dart
@@ -40,9 +40,6 @@ class _GroupChatHomeScreenState extends State<GroupChatHomeScreen> {
     });
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
